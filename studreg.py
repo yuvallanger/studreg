4444ad507fa4080868879b9717257243471ee7ee
Yuval Langer <yuval.langer@gmail.com>
2014-12-20 20:21:21 +0200

    Newest changes

--- studreg.py
+++ studreg.py
@@ -11,12 +11,12 @@ from functools import wraps
 import flask
 import flask.logging
 from flask import Flask, request
-from flask.ext import restful
-import flask.ext.restful.reqparse
 import flask.ext.sqlalchemy
+import flask.ext.restful
+import flask.ext.restful.reqparse
 
 # For the models
-from sqlalchemy import Column, Float, Integer, String, Table, Text, text
+from sqlalchemy import Float, Text, text, String, Integer, Column
 
 #######################
 #### Configuration ####
@@ -29,10 +29,10 @@ MYSQL_URI = (
     '{username}:{password}@'
     '{host}/{database}'
 ).format(
-    username=local_config.mysql['username'],
-    password=local_config.mysql['password'],
-    host=local_config.mysql['host'],
-    database=local_config.mysql['database'],
+    username = local_config.mysql['username'],
+    password = local_config.mysql['password'],
+    host     = local_config.mysql['host'    ],
+    database = local_config.mysql['database'],
 )
 
 ######################
@@ -43,7 +43,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = MYSQL_URI
 db = flask.ext.sqlalchemy.SQLAlchemy(app)
 
-api = restful.Api(app)
+api = flask.ext.restful.Api(app)
 
 logger = flask.logging.create_logger(app)
 
@@ -52,17 +52,18 @@ logger = flask.logging.create_logger(app)
 ########################
 
 def check_auth(username, password):
-    return username == 'admin' and password == 'password'
+    logger.debug((username, password))
+    authorized = local_config.api_user.get(username, None) == password
+    return authorized
 
 
 def authenticate():
-    return Response(
+    """Sends a 401 response that enables basic auth"""
+    return flask.Response(
         'Could not verify your access level for that URL.\n'
         'You have to login with proper credentials',
         401,
-        {
-            'WWW-Authenticate': 'Basic realm="Login Required"',
-        },
+        {'WWW-Authenticate': 'Basic realm="Login Required"'},
     )
 
 
@@ -70,7 +71,8 @@ def requires_auth(f):
     @wraps(f)
     def decorated(*args, **kwargs):
         auth = request.authorization
-        if not auth or check_auth(auth.username, auth.password):
+        logger.debug(auth)
+        if not auth or not check_auth(auth.username, auth.password):
             return authenticate()
         return f(*args, **kwargs)
     return decorated
@@ -86,6 +88,9 @@ class Ezor(db.Model):
     ezor = Column(Integer, nullable=False)
     hug = Column(Integer, nullable=False)
 
+class Revaha(object):
+    DOES_NOT_BELONG = u'0'
+    BELONGS = u'1'
 
 class Studentreg(db.Model):
     __tablename__ = 'studentreg'
@@ -153,48 +158,61 @@ def nocache(view):
 #### API ####
 #############
 
-parser = restful.reqparse.RequestParser()
+parser = flask.ext.restful.reqparse.RequestParser()
 parser.add_argument('identity_number', type=str, help='Student identity number')
 parser.add_argument('email', type=str, help='Student e-mail address')
 
-def find_membership_status(identity_number, email):
-    logger.debug('arguments: {}'.format((identity_number, email)))
+class AgudaMembershipTokens(object):
+    MEMBER = u'member'
+    NOT_MEMBER= u'not_member'
 
-    some_res = Studentreg.query.first()
-    logger.debug('some_res {}',format((some_res.id, some_res.email)))
+class StudentExistenceTokens(object):
+    STUDENT_EXISTS = u'student_exists'
+    STUDENT_MISSING = u'student_missing'
+
+def get_membership_status(identity_number, email):
+    """
+    :type identity_number: String
+    :type email:  String
+    :rtype : dict
+    """
+    logger.debug('arguments: {}'.format((identity_number, email)))
 
     sql_query_result = Studentreg.query.get(identity_number)
 
     # Student record exists
     if sql_query_result is None:
-        return 1
-
-    # Student email is valid
-    if sql_query_result.email.strip() != email.strip():
-        return 2
-
-    # Student doesn't belong to aguda
-    if sql_query_result.revaha == u'0':
-        return 3
-
-    # Student belongs to aguda
-    if sql_query_result.revaha == u'1':
-        return 4
-
-class MembershipStatus(restful.Resource):
-    def post(self):
-        args = parser.parse_args()
-        membership_status = find_membership_status(
-            identity_number=args['identity_number'].strip(),
-            email=args['email'].strip(),
+        return dict(
+            student_existence=StudentExistenceTokens.STUDENT_MISSING,
         )
-        return dict(membership_status=membership_status)
+
+    if sql_query_result.email.strip() != email.strip():
+        # Student does not belong to the aguda.
+        if sql_query_result.revaha == Revaha.DOES_NOT_BELONG:
+            return dict(
+                student_existence=StudentExistenceTokens.STUDENT_EXISTS,
+                aguda_membership_status=AgudaMembershipTokens.NOT_MEMBER,
+            )
+
+        # Student belongs to aguda
+        if sql_query_result.revaha == Revaha.BELONGS:
+            return dict(
+                student_existence=StudentExistenceTokens.STUDENT_EXISTS,
+                aguda_membership_status=AgudaMembershipTokens.NOT_MEMBER,
+            )
 
 
-api.add_resource(
-    MembershipStatus,
-    '/api/v1.0/membership_status/',
-)
+class MembershipStatus(flask.ext.restful.Resource):
+    @requires_auth
+    def post(self):
+        identity_number = request.form['identity_number']
+        email = request.form['email']
+        return get_membership_status(
+            identity_number=identity_number.strip(),
+            email=email.strip(),
+        )
+
+api.add_resource(MembershipStatus, '/v1.0/membership_status/')
 
 ##############
 #### Main ####
@@ -203,5 +221,5 @@ api.add_resource(
 if __name__ == '__main__':
     app.run(
         port=5001,
-        debug=True,
+        debug=local_config.debug,
     )
