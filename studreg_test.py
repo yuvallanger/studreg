7b0504881e418d25f54278920a8cca12619283e3
Yuval Langer <yuval.langer@gmail.com>
2015-02-05 00:00:24 +0200

    Adding tests

--- studreg_test.py
+++ studreg_test.py
@@ -3,13 +3,18 @@ import unittest
 import config
 import tempfile
 
+try: import simplejson as json
+except ImportError: import json
+
+import requests
+
 def make_request(data):
     username = 'copy_people'
     password = config.local_config.api_user['copy_people']
-    return request(
-        'http://127.0.0.1:5002',
+    return requests.post(
+        'https://aguda.org.il/studreg/api/beta/membership_status',
         auth=(username, password),
-        data=data
+        data=data,
     )
 
 class StudregTestCase(unittest.TestCase):
@@ -19,8 +24,33 @@ class StudregTestCase(unittest.TestCase):
     def test_failing(self):
         assert False
 
-    def login(self, username, password):
-        return self.app.post()
+    def test_no_id(self):
+        data = dict(
+            email='yuval.langer@gmail.com',
+        )
+        r = make_request(data)
+
+        result = json.loads(r.text)
+        expected = json.loads('{"student_existence": "student_missing"}')
+
+        print(result)
+        print(expected)
+
+        assert result == expected
+
+    def test_exists_and_agudaorg_member(self):
+        data = dict(
+            email='doron.zaada@mail.huji.ac.il',
+        )
+        r = make_request(data)
+
+        result = json.loads(r.text)
+        expected = json.loads('{"aguda_membership_status": "member"}')
+
+        print(result)
+        print(expected)
+
+        assert result == expected
 
 if __name__ == '__main__':
     unittest.main()
