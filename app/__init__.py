acad7b1e3c831ea101fdbbf18eac07ad8d378cc2
Yuval Langer <yuval.langer@gmail.com>
2015-02-23 17:36:24 +0200

    Adding LoginManager and OpenID

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,23 @@
+import os
+
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.script import Manager
 from flask.ext.migrate import Migrate, MigrateCommand
+from flask.ext.login import LoginManager
+from flask.ext.openid import OpenID
 
+from config import basedir
 
 app = Flask(__name__)
 app.config.from_object('config')
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+lm = LoginManager()
+lm.init_app(app)
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
 
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
