9dba9da76617d3ceed53876433b92168dda6e35c
Yuval Langer <yuval.langer@gmail.com>
2015-02-24 01:10:39 +0200

    Adding copyright notice

--- config.py
+++ config.py
@@ -1,3 +1,25 @@
+"""
+Studreg, Hafakulta's student union membership management system.
+
+Copyright (C) 2014,2015 Yuval Langer.
+
+This file is part of Studreg.
+
+Studreg is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Studreg is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with Studreg.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+
 import os
 import flask
 
